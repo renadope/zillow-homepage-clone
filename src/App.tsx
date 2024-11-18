817f2f98d2f4171df2cc0357f815421b0750844d
renadope <renadoparia@gmail.com>
2024-11-18 02:23:17 -0400

    making progress, gonna get harder in a bit

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="font-sans">
       <Nav />
-      <div className={'relative h-auto w-full'}>
-        <div className={"absolute inset-0 bg-[url('src/assets/house-1.jpg')] bg-cover bg-center bg-no-repeat"}></div>
+      <div className={'relative h-full w-full overflow-hidden'}>
+        <div className={"absolute inset-0 bg-[url('src/assets/house-2.jpg')] bg-cover bg-center bg-no-repeat"}></div>
         <div className="absolute inset-0 bg-gradient-to-br from-sky-900/50 to-sky-900/50" aria-hidden="true" />
         <div className="relative z-10 p-8 text-white">
           <div className={'mx-auto max-w-7xl px-10'}>
-            <div className={'flex flex-col gap-2 px-2 py-10'}>
+            <div className={'flex flex-col justify-center gap-2 px-2 py-16'}>
               <div className={'flex flex-1 gap-6'}>
                 <h1 className={'text-6xl font-black'}>Agents.</h1>
                 <h1 className={'text-6xl font-black'}>Tours.</h1>
@@ -20,7 +20,8 @@ function App() {
               </div>
               <div>
                 <input
-                  className={'w-5/12 rounded-xl p-4 text-sky-950 shadow-2xl'}
+                  type={'text'}
+                  className={'w-5/12 rounded-xl p-5 text-sky-950 shadow-2xl'}
                   placeholder={'Enter an address, neighborhood or Zip Code.'}
                 />
               </div>
