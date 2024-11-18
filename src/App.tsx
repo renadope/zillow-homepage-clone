4f7968628c152cca012651b07f9a5c4556f538f2
renadope <renadoparia@gmail.com>
2024-11-18 00:38:50 -0400

    adding an icon is pain

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,28 @@ function App() {
   return (
     <div className="font-sans">
       <Nav />
-      <div className={'relative h-3/4 w-full'}>
-        <div className={"absolute inset-0 bg-[url('src/assets/house-1.jpg')] bg-cover bg-center"}></div>
-        <div className="absolute inset-0 bg-gradient-to-br from-sky-500/20 to-sky-300/20" aria-hidden="true" />
+      <div className={'relative h-auto w-full'}>
+        <div className={"absolute inset-0 bg-[url('src/assets/house-1.jpg')] bg-cover bg-center bg-no-repeat"}></div>
+        <div className="absolute inset-0 bg-gradient-to-br from-sky-900/50 to-sky-900/50" aria-hidden="true" />
         <div className="relative z-10 p-8 text-white">
-          <h1>Content on top of darkened background</h1>
+          <div className={'mx-auto max-w-7xl px-10'}>
+            <div className={'flex flex-col gap-2 px-2 py-10'}>
+              <div className={'flex flex-1 gap-6'}>
+                <h1 className={'text-6xl font-black'}>Agents.</h1>
+                <h1 className={'text-6xl font-black'}>Tours.</h1>
+              </div>
+              <div className={'mb-10 flex flex-1 gap-6'}>
+                <h1 className={'text-6xl font-black'}>Loans.</h1>
+                <h1 className={'text-6xl font-black'}>Homes.</h1>
+              </div>
+              <div>
+                <input
+                  className={'w-5/12 rounded-xl p-4 shadow-2xl'}
+                  placeholder={'Enter an address, neighborhood or Zip Code.'}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
