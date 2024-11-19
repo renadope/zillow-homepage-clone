b31ea075fc5ebd2075eb59a3bd055b5721964f7a
renadope <renadoparia@gmail.com>
2024-11-19 17:11:52 -0400

    recommedation section looks good

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,42 @@ function App() {
         heading={'Check out these homes'}
         subHeading={'Continue looking at these similar homes'}
       />
+      <section className={'border-b-2 border-sky-300 bg-gradient-to-br from-sky-50 to-sky-50'}>
+        <div className={'mx-auto max-w-7xl px-10'}>
+          <div className={'flex flex-col gap-2'}>
+            <div className={'p-10'}>
+              <h6 className={'mb-5 text-center text-xl font-semibold text-sky-950'}>
+                About dwelling's recommendations.
+              </h6>
+              <p className={'text-center text-gray-500'}>
+                Recommendations are based on your location and search activity, such as the homes you've viewed and
+                saved and the filters you've used. We use this information to bring similar homes to your attention, so
+                you don't miss out.
+              </p>
+            </div>
+            <div className={'mb-5 grid gap-4 overflow-hidden py-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4'}>
+              <button className={'border-r-2 border-sky-800 text-lg'}>
+                <span className={'text-sky-950 hover:underline'}>Real Estate</span>{' '}
+                <span className={'text-sky-500'}>&darr;</span>
+              </button>
+              <button className={'border-r-2 border-sky-800 text-lg'}>
+                <span className={'text-sky-950 hover:underline'}>Rentals</span>{' '}
+                <span className={'text-sky-500'}>&darr;</span>
+              </button>
+
+              <button className={'border-r-2 border-sky-800 text-lg'}>
+                <span className={'text-sky-950 hover:underline'}>Mortage Rates</span>{' '}
+                <span className={'text-sky-500'}>&darr;</span>
+              </button>
+
+              <button className={'text-lg'}>
+                <span className={'text-sky-950 hover:underline'}>Browse Homes</span>{' '}
+                <span className={'text-sky-500'}>&darr;</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
