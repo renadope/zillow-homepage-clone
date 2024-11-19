fcbbf6592001a5be7b4965c31a5eeb0456fa88cf
renadope <renadoparia@gmail.com>
2024-11-19 18:51:33 -0400

    first layer of about section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import { houseListings } from './fake-data/house-card.ts'
 import { AboutSection } from './AboutSection.tsx'
 import { HouseCardRow } from './HouseCardRow.tsx'
 import { Features } from './Features.tsx'
+import { cn } from './lib/utils.ts'
+
+const links = [
+  'About',
+  'Destimates',
+  'Research',
+  'Careers',
+  'Careers - U.S. Privacy Notice',
+  'Careers - Mexico Privacy Notice',
+  'Help',
+  'Advertise',
+  'Fair Housing Guide',
+  'Advocacy',
+  'Terms of use',
+]
 
 function App() {
   return (
@@ -39,6 +54,34 @@ function App() {
         subHeading={'Continue looking at these similar homes'}
       />
       <AboutSection />
+      <section>
+        <div className={'mx-auto max-w-7xl px-10'}>
+          <div className={'flex flex-col'}></div>
+          <FooterLinks links={links} />
+        </div>
+      </section>
+    </div>
+  )
+}
+
+function FooterLinks({ links }: { links: string[] }) {
+  return (
+    <div className={'flex flex-wrap justify-center gap-2 py-10'}>
+      {links &&
+        links.length > 0 &&
+        links.map((item, i) => {
+          return (
+            <p
+              className={cn(
+                `cursor-pointer text-sm text-gray-500 hover:text-sky-700`,
+                `${i !== links.length - 1 ? 'border-r border-sky-200 pr-2' : ''}`
+              )}
+              key={i}
+            >
+              {item}
+            </p>
+          )
+        })}
     </div>
   )
 }
