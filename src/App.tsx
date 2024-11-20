91324e85e256fbf373edccc2004efcdfdf304d1d
renadope <renadoparia@gmail.com>
2024-11-20 00:52:14 -0400

    starting with theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,46 @@ import { HouseCardRow } from './HouseCardRow.tsx'
 import { Features } from './Features.tsx'
 import { Footer } from './Footer.tsx'
 import { shuffleArray } from './lib/utils.ts'
+import { ThemeProvider } from './theme-provider.tsx'
 
 function App() {
   return (
-    <div className="font-sans">
-      <Nav />
-      <SearchSection />
-      <HouseCardRow
-        listings={houseListings}
-        heading={'Homes For You'}
-        subHeading={'Based on homes you recently viewed'}
-      />
-      <div className={'mx-auto max-w-7xl px-10'}>
-        <div className={'mb-10 flex justify-center'}>
-          <button className={'bg-white p-4 text-lg font-semibold text-sky-700'}>
-            <span> &darr; </span> <span className={'text-center hover:underline'}>More recommended Homes</span>
-          </button>
+    <ThemeProvider>
+      <div className="font-sans">
+        <Nav />
+        <SearchSection />
+        <HouseCardRow
+          listings={houseListings}
+          heading={'Homes For You'}
+          subHeading={'Based on homes you recently viewed'}
+        />
+        <div className={'mx-auto max-w-7xl px-10'}>
+          <div className={'mb-10 flex justify-center'}>
+            <button className={'bg-white p-4 text-lg font-semibold text-sky-700'}>
+              <span> &darr; </span> <span className={'text-center hover:underline'}>More recommended Homes</span>
+            </button>
+          </div>
         </div>
+        <Features />
+        <HouseCardRow
+          listings={houseListings}
+          heading={'Homes For You'}
+          subHeading={'Based on homes you recently viewed'}
+        />
+        <HouseCardRow
+          listings={[...houseListings].reverse()}
+          heading={'Homes For You'}
+          subHeading={'Based on homes you recently viewed'}
+        />
+        <HouseCardRow
+          listings={shuffleArray(houseListings)}
+          heading={'Check out these homes'}
+          subHeading={'Continue looking at these similar homes'}
+        />
+        <AboutSection />
+        <Footer />
       </div>
-      <Features />
-      <HouseCardRow
-        listings={houseListings}
-        heading={'Homes For You'}
-        subHeading={'Based on homes you recently viewed'}
-      />
-      <HouseCardRow
-        listings={[...houseListings].reverse()}
-        heading={'Homes For You'}
-        subHeading={'Based on homes you recently viewed'}
-      />
-      <HouseCardRow
-        listings={shuffleArray(houseListings)}
-        heading={'Check out these homes'}
-        subHeading={'Continue looking at these similar homes'}
-      />
-      <AboutSection />
-      <Footer />
-    </div>
+    </ThemeProvider>
   )
 }
 
